e45aa5392eceec1f17ffc80b063a28ac9d022685
GrigAnd <andrey.grigorev03@gmail.com>
2020-09-14 14:51:59 +0300

    Updates

---
+++ precache-manifest.a471771c7096764bdf98dc4a235d8565.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b85f29af6cdc9c5666f601f20eee5a5f",
+    "url": "/VezdecodeVKMiniAppsTask/index.html"
+  },
+  {
+    "revision": "616d0f08ca6cc1686d15",
+    "url": "/VezdecodeVKMiniAppsTask/static/css/2.09e62520.chunk.css"
+  },
+  {
+    "revision": "616d0f08ca6cc1686d15",
+    "url": "/VezdecodeVKMiniAppsTask/static/js/2.9e7a564d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/VezdecodeVKMiniAppsTask/static/js/2.9e7a564d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "937879c42112af65bff9",
+    "url": "/VezdecodeVKMiniAppsTask/static/js/main.804a4b68.chunk.js"
+  },
+  {
+    "revision": "ab0140e70155d78d593d",
+    "url": "/VezdecodeVKMiniAppsTask/static/js/runtime-main.74610e5a.js"
+  }
+]);
